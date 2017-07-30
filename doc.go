233cbaa0ebba425975bd7a51bf3847704cd60340
Bredgren <brandon.edgren@gmail.com>
2017-07-29 17:00:08 -0700

    Update doc.

--- doc.go
+++ doc.go
@@ -1,25 +1,11 @@
 // Package geo contains a collection of 2-D types and other mathematical functions
 // geared towards games.
 //
-// NumGen
-//  The functions that deal with NumGen/VecGen are useful for when you want to generate
-//  random or specific series of numbers with some constraints. An example might be a
-//  particle system where, instead of taking a Vec for the initial particle velocity,
-//  it takes a VecGen supplied by the user. The particle system calls this VecGen whenever
-//  a new particle is created giving the user total control over behavior of the initial
-//  velocities.
-//
-//
-// Vec
-//  2-D Vector with many useful functions for working with it by value or reference.
-//
-// Rect
-//  Defines a rectangular area. Like Vec it also has many functions for values and references.
-//
-// Miscellaneous
-//  ...
-//
-// TODO:
-//  - circle
-//  - camera
+// Includes
+//  - Types for 2-D vector, rectangle, and circle
+//  - A collection of easing functions
+//  - Functions for generating randon numbers and vectors, including a function for randomly
+//    selecting an item from a list with weighted probabilities
+//  - Perlin noise functions
+//  - Other midcellaneous functions like Clamp, Map, and Mod
 package geo
